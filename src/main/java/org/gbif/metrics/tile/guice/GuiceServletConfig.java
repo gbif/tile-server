e22d004733ab4ed00a7544355ad3b1a9be4343b2
Federico Mendez <fmendez@gbif.org>
2014-11-10 12:15:15 +0100

    merging master

--- src/main/java/org/gbif/metrics/tile/guice/GuiceServletConfig.java
+++ src/main/java/org/gbif/metrics/tile/guice/GuiceServletConfig.java
@@ -2,12 +2,15 @@ package org.gbif.metrics.tile.guice;
 
 import org.gbif.metrics.cube.tile.density.guice.DensityCubeHBaseModule;
 import org.gbif.metrics.tile.DensityTileRenderer;
+import org.gbif.utils.file.properties.PropertiesUtil;
+import org.gbif.ws.app.ConfUtils;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 
 import com.google.common.base.Joiner;
+import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -28,7 +31,7 @@ import org.slf4j.LoggerFactory;
 public class GuiceServletConfig extends GuiceServletContextListener {
 
   private static final Logger LOG = LoggerFactory.getLogger(GuiceServletConfig.class);
-  private static final String APPLICATION_PROPERTIES = "/application.properties";
+  private static final String APPLICATION_PROPERTIES = "application.properties";
   private static final String PROP_STORAGE_TYPE = "tile-server.cube.harness";
   private static final String PROP_CSV = "tile-server.csv.location";
   private static final String PROP_ZOOMS = "tile-server.csv.numberOfZooms";
@@ -40,39 +43,37 @@ public class GuiceServletConfig extends GuiceServletContextListener {
 
   @Override
   protected Injector getInjector() {
-    Properties p = new Properties();
     try {
-      p.load(GuiceServletConfig.class.getResourceAsStream(APPLICATION_PROPERTIES));
-    } catch (IOException e) {
-      throw new IllegalArgumentException(APPLICATION_PROPERTIES + " is missing");
-    }
-
-    List<Module> modules = Lists.newArrayList();
-    modules.add(new InstrumentationModule());
-    modules.add(new MetricsModule(p));
-    if (TYPE_CSV_MEMORY.equals(p.getProperty(PROP_STORAGE_TYPE))) {
-      LOG.info("Configuration declares a CSV in-memory DataCube");
-      int zooms = getInt(p, PROP_ZOOMS, DEFAULT_ZOOMS);
-      int pixelsPerCluster = getInt(p, PROP_PIXELS_PER_CLUSTER, DEFAULT_PIXELS_PER_CLUSTER);
-      modules.add(new InMemoryTileModule(p.get(PROP_CSV).toString(), zooms, pixelsPerCluster));
-    } else if (TYPE_HBASE.equals(p.getProperty(PROP_STORAGE_TYPE))) {
-      LOG.info("Configuration declares an HBase backed DataCube");
-      modules.add(new DensityCubeHBaseModule(p));
-    } else {
-      throw new RuntimeException(PROP_STORAGE_TYPE + " is not a valid.  Use one of " + Joiner.on(",").join(TYPE_CSV_MEMORY, TYPE_HBASE));
-    }
-    modules.add(new ServletModule() {
-
-      @Override
-      protected void configureServlets() {
-        serve("/density/*").with(DensityTileRenderer.class);
+      final Properties properties = PropertiesUtil.readFromFile(ConfUtils.getAppConfFile(APPLICATION_PROPERTIES));
+      List<Module> modules = Lists.newArrayList();
+      modules.add(new InstrumentationModule());
+      modules.add(new MetricsModule(properties));
+      if (TYPE_CSV_MEMORY.equals(properties.getProperty(PROP_STORAGE_TYPE))) {
+        LOG.info("Configuration declares a CSV in-memory DataCube");
+        int zooms = getInt(properties, PROP_ZOOMS, DEFAULT_ZOOMS);
+        int pixelsPerCluster = getInt(properties, PROP_PIXELS_PER_CLUSTER, DEFAULT_PIXELS_PER_CLUSTER);
+        modules.add(new InMemoryTileModule(properties.get(PROP_CSV).toString(), zooms, pixelsPerCluster));
+      } else if (TYPE_HBASE.equals(properties.getProperty(PROP_STORAGE_TYPE))) {
+        LOG.info("Configuration declares an HBase backed DataCube");
+        modules.add(new DensityCubeHBaseModule(properties));
+      } else {
+        throw new RuntimeException(PROP_STORAGE_TYPE + " is not a valid. Use one of " + Joiner.on(",")
+          .join(TYPE_CSV_MEMORY, TYPE_HBASE));
       }
-    });
-
-    return Guice.createInjector(modules);
+      modules.add(new ServletModule() {
+        @Override
+        protected void configureServlets() {
+          serve("/density/*").with(DensityTileRenderer.class);
+        }
+      });
+      return Guice.createInjector(modules);
+    } catch(Exception ex){
+      Throwables.propagate(ex);
+    }
+    throw new IllegalStateException("Error initiating web application");
   }
 
-  private int getInt(Properties p, String key, int defaultValue) {
+  private static int getInt(Properties p, String key, int defaultValue) {
     try {
       return (p.getProperty(key) == null) ? defaultValue : Integer.parseInt(p.getProperty(key));
     } catch (NumberFormatException e1) {
